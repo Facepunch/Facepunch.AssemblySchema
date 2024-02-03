8224b9b8006036a45cf17484488072fb393d9ca9
Garry Newman <garrynewman@users.noreply.github.com>
2024-02-03 13:36:11 +0000

    Fix assembly resolve

--- Src/Builder.cs
+++ Src/Builder.cs
@@ -1,25 +1,21 @@
-﻿global using Mono.Cecil;
+﻿namespace Facepunch.AssemblySchema;
 
-using Mono.Collections.Generic;
-
-namespace Facepunch.AssemblySchema;
-
-public partial class Builder
+public partial class Builder : IAssemblyResolver
 {
 	List<AssemblyDefinition> Assemblies { get; } = new();
 	List<XmlDocumentation> Documentation { get; } = new();
 
-	public void AddAssembly( byte[] assemblyData )
+	public void AddAssembly(byte[] assemblyData)
 	{
-		using var ms = new MemoryStream( assemblyData );
-		var assembly = AssemblyDefinition.ReadAssembly( ms, new Mono.Cecil.ReaderParameters { InMemory = true, ReadingMode = Mono.Cecil.ReadingMode.Immediate } );
-		Assemblies.Add( assembly );
+		using var ms = new MemoryStream(assemblyData);
+		var assembly = AssemblyDefinition.ReadAssembly(ms, new Mono.Cecil.ReaderParameters { AssemblyResolver = this, InMemory = true, ReadingMode = Mono.Cecil.ReadingMode.Deferred });
+		Assemblies.Add(assembly);
 	}
 
-	public void AddDocumentation( byte[] bytes )
+	public void AddDocumentation(byte[] bytes)
 	{
-		var d = AssemblySchema.XmlDocumentation.ReadFromString( bytes );
-		Documentation.Add( d );
+		var d = AssemblySchema.XmlDocumentation.ReadFromString(bytes);
+		Documentation.Add(d);
 	}
 
 	/// <summary>
@@ -30,17 +26,38 @@ public partial class Builder
 		var info = new Schema();
 		info.Types ??= new();
 
-		foreach ( var a in Assemblies )
+		foreach (var a in Assemblies)
 		{
-			ProcessAssembly( info, a );
+			ProcessAssembly(info, a);
 		}
 
-		foreach ( var t in info.Types )
+		foreach (var t in info.Types)
 		{
-			t.Polish( info );
+			t.Polish(info);
 		}
 
 		return info;
+	}
+
+	public void Dispose()
+	{
+		foreach (var a in Assemblies)
+			a.Dispose();
+
+		Assemblies.Clear();
+		Documentation.Clear();
+
+		return;
+	}
+
+	public AssemblyDefinition Resolve(AssemblyNameReference name)
+	{
+		return Assemblies.FirstOrDefault(x => x.Name.Name == name.Name);
+	}
+
+	public AssemblyDefinition Resolve(AssemblyNameReference name, ReaderParameters parameters)
+	{
+		return Resolve(name);
 	}
 }
 
