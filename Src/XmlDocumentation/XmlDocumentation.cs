870ef54a8122771b79644812fcfd7e3701838cf4
Garry Newman <garrynewman@users.noreply.github.com>
2025-05-30 20:02:58 +0100

    Decode special entities for plain text

--- Src/XmlDocumentation/XmlDocumentation.cs
+++ Src/XmlDocumentation/XmlDocumentation.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Net;
+using System.Text.RegularExpressions;
 using System.Xml;
 
 namespace Facepunch.AssemblySchema;
@@ -7,19 +8,19 @@ public class XmlDocumentation
 {
 	public Dictionary<string, Documentation> Entries = new Dictionary<string, Documentation>();
 
-	internal static XmlDocumentation ReadFromString(byte[] xmlSource)
+	internal static XmlDocumentation ReadFromString( byte[] xmlSource )
 	{
-		using var stream = new MemoryStream(xmlSource);
+		using var stream = new MemoryStream( xmlSource );
 
 		var doc = new XmlDocumentation();
 
 		var xml = new XmlDocument();
-		xml.Load(stream);
+		xml.Load( stream );
 
-		foreach (XmlNode m in xml.SelectSingleNode(".//members"))
+		foreach ( XmlNode m in xml.SelectSingleNode( ".//members" ) )
 		{
-			var d = Documentation.ParseFromNode(m);
-			if (d is null) continue;
+			var d = Documentation.ParseFromNode( m );
+			if ( d is null ) continue;
 
 			doc.Entries[d.Name] = d;
 		}
@@ -28,16 +29,19 @@ public class XmlDocumentation
 
 	}
 
-	public static string XmlToPlainText(string xml)
+	public static string XmlToPlainText( string xml )
 	{
-		if (xml is null)
+		if ( xml is null )
 			return default;
 
 		// Remove HTML tags
-		xml = Regex.Replace(xml, "<a href=\"[^\"]*\">(.*?)</a>", "$1", RegexOptions.IgnoreCase);
+		xml = Regex.Replace( xml, "<a href=\"[^\"]*\">(.*?)</a>", "$1", RegexOptions.IgnoreCase );
 
 		// Replace <see cref="..."/> with its cref value
-		xml = Regex.Replace(xml, "<see cref=\"([A-Z]\\:)?([^\"]*)\"\\s*/>", "$2", RegexOptions.IgnoreCase);
+		xml = Regex.Replace( xml, "<see cref=\"([A-Z]\\:)?([^\"]*)\"\\s*/>", "$2", RegexOptions.IgnoreCase );
+
+		// decode entities
+		xml = WebUtility.HtmlDecode( xml );
 
 		return xml;
 	}
